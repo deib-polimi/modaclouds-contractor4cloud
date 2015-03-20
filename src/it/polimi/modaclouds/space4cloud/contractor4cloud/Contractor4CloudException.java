58e94f048cbf574c2571fcca303eb08f49920a65
Riccardo B. Desantis <effetti@gmail.com>
2015-03-20 18:22:21 +0100

    Ups!

---
+++ src/it/polimi/modaclouds/space4cloud/contractor4cloud/Contractor4CloudException.java
@@ -0,0 +1,18 @@
+package it.polimi.modaclouds.space4cloud.contractor4cloud;
+
+public class Contractor4CloudException extends Exception {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6072605019115908372L;
+
+	public Contractor4CloudException(String string, Throwable cause) {
+		super(string, cause);
+	}
+	
+	public Contractor4CloudException(String string) {
+		super(string);
+	}
+
+}
