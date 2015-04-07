3e9f7daa2f685f0c0957c92ef7f59c4acd2d7165
Riccardo B. Desantis <effetti@gmail.com>
2015-04-07 13:46:54 +0200

    Exporting an int directly as a string is a no-no, it seems.

--- src/it/polimi/modaclouds/space4cloud/contractor4cloud/Configuration.java
+++ src/it/polimi/modaclouds/space4cloud/contractor4cloud/Configuration.java
@@ -109,7 +109,7 @@ public class Configuration {
 		
 		prop.put("RUN_SOLVER_CMPL", RUN_SOLVER);
 		prop.put("RUN_CMPL_FOLDER", RUN_CMPL_FOLDER);
-		prop.put("CMPL_THREADS", CMPL_THREADS);
+		prop.put("CMPL_THREADS", String.valueOf(CMPL_THREADS));
 		
 		prop.put("MATH_SOLVER", MATH_SOLVER.getName());
 		
@@ -144,7 +144,7 @@ public class Configuration {
 		
 		prop.put("RUN_SOLVER_CMPL", RUN_SOLVER);
 		prop.put("RUN_CMPL_FOLDER", RUN_CMPL_FOLDER);
-		prop.put("CMPL_THREADS", CMPL_THREADS);
+		prop.put("CMPL_THREADS", String.valueOf(CMPL_THREADS));
 		
 		prop.put("MATH_SOLVER", MATH_SOLVER.getName());
 		
